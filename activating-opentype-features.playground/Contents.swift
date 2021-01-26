69718fb3624b558150ee13eca01a7dc3882bd328
Mickaël Floc’hlay <mickael.flochlay@memo.bank>
2021-01-26 17:27:53 +0100

    feat: add wrapper and export to PNG with code

--- activating-opentype-features.playground/Contents.swift
+++ activating-opentype-features.playground/Contents.swift
@@ -1,5 +1,7 @@
 //: Activating OpenType Features in iOS
 
+// 📜 Read the blog post at: https://bootstragram.com/blog/activating-open-type-features-ios/
+
 import PlaygroundSupport
 import UIKit
 
@@ -37,6 +39,23 @@ extension UIFont {
     }
 }
 
+extension UIView {
+    func asImage() -> UIImage {
+        let renderer = UIGraphicsImageRenderer(bounds: bounds)
+        return renderer.image { rendererContext in
+            layer.render(in: rendererContext.cgContext)
+        }
+    }
+}
+
+extension UIImage {
+    func saveAs() {
+        let destinationURL = FileManager.default.temporaryDirectory.appendingPathComponent("playground-snapshot.png")
+        try! self.pngData()?.write(to: destinationURL)
+        print("Written to \(destinationURL.path)")
+    }
+}
+
 UIFont.printAllFontNames()
 UIFont(name: "Inter-Bold", size: 16.0)?.listFeatures()
 
@@ -71,6 +90,8 @@ func createFontWithOptions(fontName: String, size: CGFloat) -> UIFont {
 }
 
 class MyViewController: UIViewController {
+    let wrapper = UIView()
+
     override func loadView() {
         let view = UIView()
         view.backgroundColor = .white
@@ -79,25 +100,41 @@ class MyViewController: UIViewController {
         label.translatesAutoresizingMaskIntoConstraints = false
         label.font = UIFont(name: "Inter-Bold", size: 32.0)
         label.text = "0123456789"
-        label.textColor = .black
+        label.textColor = .white
 
         let labelWithFeatures = UILabel()
         labelWithFeatures.translatesAutoresizingMaskIntoConstraints = false
         labelWithFeatures.font = createFontWithOptions(fontName: "Inter-Bold", size: 32.0)
         labelWithFeatures.text = "0123456789"
-        labelWithFeatures.textColor = .black
+        labelWithFeatures.textColor = .init(hue: 55.0/360.0, saturation: 0.57, brightness: 0.98, alpha: 1.0)
 
-        view.addSubview(label)
-        view.addSubview(labelWithFeatures)
+        wrapper.translatesAutoresizingMaskIntoConstraints = false
+        wrapper.layer.cornerRadius = 8.0
+        wrapper.layer.cornerCurve = .continuous
+        wrapper.backgroundColor = .init(hue: 221.0/360.0, saturation: 0.32, brightness: 0.30, alpha: 1.0)
+
+        view.addSubview(wrapper)
+        wrapper.addSubview(label)
+        wrapper.addSubview(labelWithFeatures)
 
         NSLayoutConstraint.activate([
-            label.firstBaselineAnchor.constraint(equalToSystemSpacingBelow: view.topAnchor, multiplier: 1.0),
+            wrapper.topAnchor.constraint(equalToSystemSpacingBelow: view.topAnchor, multiplier: 1.0),
+
+            label.firstBaselineAnchor.constraint(equalToSystemSpacingBelow: wrapper.topAnchor, multiplier: 1.0),
             label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             labelWithFeatures.firstBaselineAnchor.constraint(equalToSystemSpacingBelow: label.lastBaselineAnchor, multiplier: 1.0),
             labelWithFeatures.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            wrapper.bottomAnchor.constraint(equalToSystemSpacingBelow: labelWithFeatures.lastBaselineAnchor, multiplier: 1.0),
+            label.leadingAnchor.constraint(equalToSystemSpacingAfter: wrapper.leadingAnchor, multiplier: 2.0),
+            wrapper.trailingAnchor.constraint(equalToSystemSpacingAfter: label.trailingAnchor, multiplier: 2.0)
         ])
 
         self.view = view
+    }
+    
+    override func viewDidLayoutSubviews() {
+        wrapper.asImage().saveAs()
     }
 }
 
